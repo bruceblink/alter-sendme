27075daf413722d5837c1dec4a907b18229ada93
likanug <likanug.g@qq.com>
2026-01-21 14:35:40 +0800

    feat(ui): disable right click menu in UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n'
-import { initializePlatformStyles } from './lib/platformStyles'
+import {initializePlatformStyles} from './lib/platformStyles'
 
 initializePlatformStyles()
+
+// prevent right click menu
+document.addEventListener('contextmenu', (e) => e.preventDefault());
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
