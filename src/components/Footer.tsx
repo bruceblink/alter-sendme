7efc45483954a226253d7e41d20bcc2bee4907d9
likanug <likanug.g@qq.com>
2026-01-28 21:31:13 +0800

    refactor: update the appVersion retrieval logic in Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,25 @@
 import {REPOSITORY_URL, SPONSORING_URL} from "@/lib/author.ts";
-import {VERSION_DISPLAY} from "@/lib/version.ts";
 import {openUrl} from "@tauri-apps/plugin-opener";
 import {LanguageSwitcher} from "@/components/LanguageSwitcher.tsx";
 import {useTranslation} from "@/i18n";
+import {useEffect, useState} from "react";
+import {getVersion} from "@tauri-apps/api/app";
 
 export function Footer() {
+    const [appVersion, setAppVersion] = useState('.....');
     const {t} = useTranslation()
+
+    useEffect(() => {
+        void getVersion().then(setAppVersion);
+    }, []);
 
     return (
         <div className="w-full h-10 text-center text-xs flex items-center justify-center relative">
         <span>
             <a target="_blank"
-               href={`${REPOSITORY_URL}/releases/tag/${VERSION_DISPLAY}`}
+               href={`${REPOSITORY_URL}/releases/tag/v${appVersion}`}
                className="btn text-sm ml-1 font-mono">
-                {VERSION_DISPLAY}
+                v{appVersion}
             </a>
         </span>
             <button
